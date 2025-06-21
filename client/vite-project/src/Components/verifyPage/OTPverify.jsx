e75b272d262460cf86acaa5dd7205078151c4ed9
Muntaha4356 <muntahapolaris@gmail.com>
2025-06-21 23:02:41 +0500

    Adding routes

---
+++ client/vite-project/src/Components/verifyPage/OTPverify.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OTPverify = () => {
+  return (
+    <div>
+      <h1>ResetPages</h1>
+    </div>
+  )
+}
+
+export default OTPverify
---
+++ client/vite-project/src/Components/verifyPage/emailVerify.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const emailVerify = () => {
+  return (
+    <div>
+      <h1>EmailVerifys</h1>
+    </div>
+  )
+}
+
+export default emailVerify
